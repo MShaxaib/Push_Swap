ac7c21f56b3d55e36bd0ce0c0b72e7ecff7b0e4d
MShaxaib <muhammad.shazaibb@gmail.com>
2024-02-04 18:39:06 +0400

    pseudo code for calculate costs

--- src/mech_turk.c
+++ src/mech_turk.c
@@ -3,62 +3,61 @@
 
 t_node  *find_smaller_target(t_node *current_a, t_node **targets, t_stack *b, int i)
 {
-    t_node  *target;
-    t_node  *current_b;
-    int     i;
+	t_node  *target;
+	t_node  *current_b;
+	int     i;
 
-    current_b = b->head;
-    target = NULL;
+	current_b = b->head;
+	target = NULL;
 
-    while(current_b != NULL)
-    {
-        if((current_b->content < current_a->content &&((target != NULL) || (current_b->content > target->content))))
-            current_b = target;
-        current_b = current_b->previous;
-    }
-    targets[i] = target;
-    if(target = NULL)
-        return(find_max(b));
-    
-    return(target);
-
-    /* 
-        1) set current b to the head of stack b
-        2) set target to NULL
-        while (current b != NULL)
-        {
-            if(current_b->content < current_a->content && (targetnode != NULL || current_b->content > targetnode->content))
-                current_b = targetnode;
-            current_b = current_b->previous;
-        }
-        if (the target is still null)
-            return the max element
-        return target
-    */
+	while(current_b != NULL)
+	{
+		if((current_b->content < current_a->content &&((target != NULL) || (current_b->content > target->content))))
+			current_b = target;
+		current_b = current_b->previous;
+	}
+	targets[i] = target;
+	if(target = NULL)
+		return(find_max(b));
+	
+	return(target);
 }
-/*
-    steps:
-    1) find targets of a 
 
-*/
+int calculate_cost(t_node *target, t_stack *stk, int index)
+{
+	/*
+	 ^	 check if the target node is at the same index as the src
+	 ^ 		if(yes) then just push A -> B
+	 ^		else check if its above or below the median 
+	 ^		if(above)
+	 ^			then keep doing RB until it reaches the top
+	 &			cost = index 
+	 ^		if(below)
+	 ^			keep doing RRB until it reaches the top
+	 &			cost = index - length
+	 ! 		once the target node reaches the top then we have to move the src node to the top as well
+	 ^ 		same thing as above for SRC stack but it will be simpler since there are only 3 elements
+
+	&		everytime we do a move we count++ and thats the cost
+	*/
+
+}
+
 void    mechanical_turk(t_stack *a, t_stack *b)
 {
-    // find target
-    t_node  *current_a;
-    t_node  **targets;
-    int     *weights;
-    int     i;
+	t_node  *current_a;
+	t_node  **targets;
+	int     *cost;
+	int     i;
 
-    i = 0;
-    //malloc for targets
-    targets = (t_node **)malloc(stk_length(a) * (sizeof(t_node *)));
-    weights = malloc(sizeof(int));
-    //malloc for weights
+	i = 0;
+	targets = (t_node **)malloc(stk_length(a) * (sizeof(t_node *)));
+	cost = malloc(sizeof(int));
 
-    while(current_a != NULL)
-    {
-        targets[i] = find_smaller_target(current_a, targets, b);
-        i++;
-        current_a = current_a->previous;
-    }   
+	while(current_a != NULL)
+	{
+		targets[i] = find_smaller_target(current_a, targets, b, i);
+		i++;
+		current_a = current_a->previous;
+	}
 }
