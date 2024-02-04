74b694364f87c61a0c3a1571e80f7746d6ac2be4
MShaxaib <139437420+MShaxaib@users.noreply.github.com>
2024-02-04 12:30:42 +0000

    latest and the greatest

--- src/mech_turk.c
+++ src/mech_turk.c
@@ -1,13 +1,28 @@
 
 #include "push_swap.h"
 
-t_node  *find_smaller_target(t_node *current_a, t_node **targets, t_stack *b)
+t_node  *find_smaller_target(t_node *current_a, t_node **targets, t_stack *b, int i)
 {
     t_node  *target;
     t_node  *current_b;
     int     i;
 
-    /*
+    current_b = b->head;
+    target = NULL;
+
+    while(current_b != NULL)
+    {
+        if((current_b->content < current_a->content &&((target != NULL) || (current_b->content > target->content))))
+            current_b = target;
+        current_b = current_b->previous;
+    }
+    targets[i] = target;
+    if(target = NULL)
+        return(find_max(b));
+    
+    return(target);
+
+    /* 
         1) set current b to the head of stack b
         2) set target to NULL
         while (current b != NULL)
@@ -20,7 +35,6 @@ t_node  *find_smaller_target(t_node *current_a, t_node **targets, t_stack *b)
             return the max element
         return target
     */
-    return (target);
 }
 /*
     steps:
@@ -38,16 +52,13 @@ void    mechanical_turk(t_stack *a, t_stack *b)
     i = 0;
     //malloc for targets
     targets = (t_node **)malloc(stk_length(a) * (sizeof(t_node *)));
+    weights = malloc(sizeof(int));
     //malloc for weights
-    /*
-        loop through a with c        urrent a
-        {
-            find a target for every element in a:
-            targets[i] = find smaller target;
-            i ++;
-            current a = current a previous
-        }
-    
-    
-    */    
+
+    while(current_a != NULL)
+    {
+        targets[i] = find_smaller_target(current_a, targets, b);
+        i++;
+        current_a = current_a->previous;
+    }   
 }
