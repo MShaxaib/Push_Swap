e6089f29464a25e71466d3976c7f50f99efcdea1
MShaxaib <muhammad.shazaibb@gmail.com>
2024-02-14 23:05:34 +0400

    asdhaksldhl

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: mshazaib <mshazaib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 20:49:57 by mshazaib          #+#    #+#             */
-/*   Updated: 2024/02/14 21:22:19 by mshazaib         ###   ########.fr       */
+/*   Updated: 2024/02/14 23:01:57 by mshazaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@ void not_sorted(t_stack *stk, t_node *iter)
 	i = 0;
 	if((float)(iter->index) / (float)stk_length(stk) >= 0.5 )
 	{
-		diff = iter->index + 1;
+		diff = (stk_length(stk) - iter->index);
 		while(i < diff)
 		{
-			rotate_stack(stk, 'a');
+			rev_rotate(stk, 'a');
 			i++;
 		}
 	}
 	else
 	{
-		diff = ((stk_length(stk) - 1) - iter->index);
+		diff = iter->index;
 		while(i < diff)
 		{
-			rev_rotate(stk, 'a');
+			rotate_stack(stk, 'a');
 			i++;
 		}
 	}
@@ -43,16 +43,20 @@ void sort_stack(t_stack *stk)
 {
 	t_node *iter;
 
-	iter = stk->head;
-	while(iter->previous != NULL)
-	{
-		if(iter->content < iter->previous->content)
-			iter = iter->previous;
-		else
-		{
-			not_sorted(stk, iter);
-			break;	
-		}
-	}
+	// printf("%d | %d | %d\n", iter->content, iter->index, stk_length(stk));
+	// iter = stk->head;
+	// while(iter != NULL)
+	// {
+	// 	if(iter->content < iter->previous->content)
+	// 		iter = iter->previous;
+	// 	else
+	// 	{
+	// 		not_sorted(stk, iter);
+	// 		break;	
+	// 	}
+	// }
 	
+	iter = find_min(stk);
+	if (iter->index)
+		not_sorted(stk, iter);
 }
